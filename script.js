ed8fbb6a8f085aaea7de13026702a9bf05b95cc0
GabrielScarpat <gabrielscarpat@icloud.com>
2026-05-20 12:18:49 -0300

    ajustando alinhamento e preferencia tema

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const botao = document.getElementById('toggle-tema');
 
-const temaSalvo = localStorage.getItem('tema');
-const sistemaClaro = window.matchMedia('(prefers-color-scheme: light)').matches;
-
-if (temaSalvo === 'claro' || (temaSalvo === null && sistemaClaro)) {
-    document.body.classList.add('tema-claro');
+if (document.documentElement.classList.contains('tema-claro')) {
     botao.textContent = '🌑';
 }
 
